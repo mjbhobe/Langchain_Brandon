2d750ec9373f91eade8c258114ac57318267a9bc
Manish Bhobe <mjbhobe@gmail.com>
2025-05-15 14:13:27 +0000

    New Code Files

---
+++ 01_ChatModels/1_chat_model_basic.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from rich.console import Console
+from rich.markdown import Markdown
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# load all environment variables
+load_dotenv()
+
+# create my LLM - using Google Gemini
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+console = Console()
+
+response = model.invoke("What is 81 divided by 9")
+console.print(f"Full response:")
+console.print(response)
+console.print(f"Just the content: {response.content}")
